3c4a12d5f3c2aa86974db2c1fc3502f268fb63c5
Michele_Felici <michelefelice58@gmail.com>
2023-02-28 17:00:43 +0100

    modified db name and organized the connection

--- conn.php
+++ conn.php
@@ -1,8 +1,17 @@
 <?php
-    $dbhost="localhost";
-    $user="admin";
-    $userpss="";
-    $db="es2";
+    $dbhost="localhost"; //indirizzo mysql
+    $user="root";       //utente phpmyadmin
+    $userpss="";         //password phpmyadmin
+    $db="pranzo";          //Nome tabella phpmyadmin
+
+
     $conn=mysqli_connect($dbhost,$user,$userpss,$db);
+    
+    // verifica della connessione
+    if (!$conn) {
+        die("Connessione fallita: " . mysqli_connect_error());
+    }
+    // echo "Connessione riuscita!";
+
 ?>
 
