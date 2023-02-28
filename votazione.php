1806e8f920589951d7fb24ff09578aa78317a12e
Michele_Felici <michelefelice58@gmail.com>
2023-02-28 18:15:19 +0100

    inverted content from index.php to votazione.php

--- votazione.php
+++ votazione.php
@@ -1,9 +1,24 @@
 <?php
     include("conn.php");
-
-    $query="SELECT * FROM ristoranti";
-    $ris=mysqli_query($conn, $query);
-    while($row=mysqli_fetch_array($ris)){
-        echo"<h1>".$row['nome']." = "."</h1>"
-    }
 ?>
+<form method="POST">
+        <select required nane='scelta'>
+            <?php
+                $query="SELECT * FROM ristoranti";
+                $ris=mysqli_query($conn, $query);
+                while($row=mysqli_fetch_array($ris)){
+                    echo "<option value=".$row['id'].">".$row['nome']."</option>";
+                }
+            ?>
+        </select>
+        <button class="pulsante" type="submit">Invia</button>
+    </form>
+    <?php
+        if(isset($_POST['scelta'])){
+            $query="SELECT voti_tot FROM ristoranti WHERE id='{$_POST['scelta']}'";
+            $row=mysqli_fetch_array(mysqli_query($conn, $query));
+            $temp=$row['voti_tot']+1;
+            $query="UPDATE ristoranti SET voti_tot='{$temp}' where id='{$_POST['scelta']}'";
+            mysqli_query($conn, $query);
+        }
+    ?>
