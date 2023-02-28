7a3c5775907155a7a132d94ea2bff148e4116c49
Michele_Felici <michelefelice58@gmail.com>
2023-02-28 17:01:18 +0100

    edited index.php

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Votazioni per pranzo dei 100 giorni</title>
 </head>
 <body>
-    
+    <?php
+        include_once('risultati.php');
+    ?>
 </body>
 </html>
