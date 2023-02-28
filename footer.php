042cf692a2d274c61f0db63f4fafca9d4aa637ed
Michele_Felici <michelefelice58@gmail.com>
2023-02-28 19:57:17 +0100

    added link to repository

--- footer.php
+++ footer.php
@@ -5,5 +5,7 @@
         <a href="https://andreianghi.ddns.net/">Andrei anghi</a>
         <br>
         <a href="https://ripasso.ddns.net/">Michele felici</a>
+        <br>
+        forka il nostro <a id="repo" href="https://github.com/AmadeusKurisuDev/VoteLunch">progetto su github</a>!
     </h1>
 </div>
