ac3fccbf2677d60aa0724d8d969461f27c89c6fa
anglyregna <andyvalycris@gmail.com>
2023-02-28 18:39:40 +0100

    add my part of work.

--- votazione.php
+++ votazione.php
@@ -2,12 +2,12 @@
     include("conn.php");
 ?>
 <form method="POST">
-        <select required nane='scelta'>
+        <select required name='scelta'>
             <?php
                 $query="SELECT * FROM ristoranti";
                 $ris=mysqli_query($conn, $query);
                 while($row=mysqli_fetch_array($ris)){
-                    echo "<option value=".$row['id'].">".$row['nome']."</option>";
+                    echo "<option>".$row['nome']."</option>";
                 }
             ?>
         </select>
@@ -15,10 +15,11 @@
     </form>
     <?php
         if(isset($_POST['scelta'])){
-            $query="SELECT voti_tot FROM ristoranti WHERE id='{$_POST['scelta']}'";
+            $query="SELECT voti_tot FROM ristoranti WHERE nome='{$_POST['scelta']}'";
             $row=mysqli_fetch_array(mysqli_query($conn, $query));
             $temp=$row['voti_tot']+1;
-            $query="UPDATE ristoranti SET voti_tot='{$temp}' where id='{$_POST['scelta']}'";
+            $query="UPDATE ristoranti SET voti_tot={$temp} where nome='{$_POST['scelta']}'";
             mysqli_query($conn, $query);
+            echo '<h1>La tua opignione e stata ricevuta (non) con successo.</h1><p>Comunque non ci interessa di cosa tu penzi</p>';
         }
     ?>
