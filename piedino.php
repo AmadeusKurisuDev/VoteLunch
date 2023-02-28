afc5f5be6dfd4cb59e76e9edc42d35c3b7bfd388
anglyregna <andyvalycris@gmail.com>
2023-02-28 19:18:32 +0100

    update my work on votazione.php

---
+++ piedino.php
@@ -0,0 +1,9 @@
+<div  class="header">
+    <h1>
+        Sito a cura di:
+        <br>
+        <a href="https://andreianghi.ddns.net/">Andrei anghi</a>
+        <br>
+        <a href="https://ripasso.ddns.net/">Michele felici</a>
+    </h1>
+</div>
