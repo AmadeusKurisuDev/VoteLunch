92ec405d7ea654c76e117e175e19d949ebabae19
anglyregna <andyvalycris@gmail.com>
2023-02-28 13:56:15 +0100

    odified risultati.php,index.php

--- index.php
+++ index.php
@@ -6,7 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<?php
+    include("conn.php");
+?>
 <body>
-    
+    <form method="POST">
+        <select>
+            <?php
+                $query="SELECT * FROM ristoranti";
+                $ris=mysqli_query($conn, $query);
+                while($row=mysqli_fetch_array($ris)){
+                    echo"<option>".$row['nome']."</option>"
+                }
+            ?>
+        </select>
+    </form>
 </body>
 </html>
